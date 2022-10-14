9952949446457cc219504c05db10be4d33037470
RusElfimov <ruselfimovbox@outlook.com>
2022-10-14 17:05:54 +0300

    work2

---
+++ work2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Подсчитать сумму цифр в числе.
+Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int res = 0;
+while (number/ 10 != 0 || number % 10 != 0)
+{
+    res = res + (number % 10);
+    number = number / 10;
+}
+if (res < 0) res=res*-1;
+Console.WriteLine($"Сумма чисел {res}");
