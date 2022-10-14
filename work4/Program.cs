9feac8fbbaf06eb1d5082ec225560944af0860cb
RusElfimov <ruselfimovbox@outlook.com>
2022-10-14 17:14:47 +0300

    work4

---
+++ work4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Write("Введите число:");
+int Chislo = int.Parse(Console.ReadLine() ?? "0");
+int Kub_Chisla;
+for (int i=1; i<=Chislo;i++)
+{
+Kub_Chisla = i*i*i;
+if ((Kub_Chisla%10)%2==0) Console.Write($"{Kub_Chisla} ");
+};
