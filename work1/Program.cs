486cb2126814d0d6bc1e9bc276da460f875c9671
RusElfimov <ruselfimovbox@outlook.com>
2022-10-14 17:01:05 +0300

    work1

---
+++ work1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Найти кубы чисел от 1 до N.
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int count = 1;
+while (count <= number)
+{
+    int res = count*count*count;
+    Console.WriteLine($"{res} куб {count}");
+    count++;
+}
