48f1ff52da3d00cdf18626d031db0f1166f07175
RusElfimov <ruselfimovbox@outlook.com>
2022-10-14 17:11:41 +0300

    work3

---
+++ work3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int sch = 1;
+for (int i=1; i<=number;i++)
+{
+sch = sch * i;
+Console.WriteLine(sch);
+}
